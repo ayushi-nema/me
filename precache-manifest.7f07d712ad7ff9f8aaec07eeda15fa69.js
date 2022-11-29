bda36b7a4b5b087d19be1bf545bb667fef19aa93
sangam <sangam.kumar@climate-connect.com>
2022-11-29 11:57:10 +0530

    Updates

---
+++ precache-manifest.7f07d712ad7ff9f8aaec07eeda15fa69.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6fd804bcddfcebd5f0041adcd2e851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df08b7333832bac1a2b3",
+    "url": "/static/css/3.7d47e3ca.chunk.css"
+  },
+  {
+    "revision": "d8c6be0d0dd669465be3",
+    "url": "/static/css/4.274b9066.chunk.css"
+  },
+  {
+    "revision": "eff870f89c8f82a02429",
+    "url": "/static/css/main.724a8d12.chunk.css"
+  },
+  {
+    "revision": "ff7d7ce0774617c187cc",
+    "url": "/static/js/2.96f43e73.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.96f43e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df08b7333832bac1a2b3",
+    "url": "/static/js/3.95c57b7b.chunk.js"
+  },
+  {
+    "revision": "d8c6be0d0dd669465be3",
+    "url": "/static/js/4.3e00d3b3.chunk.js"
+  },
+  {
+    "revision": "eff870f89c8f82a02429",
+    "url": "/static/js/main.386f82cb.chunk.js"
+  },
+  {
+    "revision": "aac0ae28b378ab3a97fd",
+    "url": "/static/js/runtime-main.56dbdda1.js"
+  },
+  {
+    "revision": "55e54b638bd5936eb331e8de66352765",
+    "url": "/static/media/AYUSHI_NEMA_SE_2yrs_CV.55e54b63.pdf"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4ea9019750201f36a301b812cb7cc015",
+    "url": "/static/media/CDGI Logo.4ea90197.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3c8a9172184e76c4bab1bbbf3f72a2ba",
+    "url": "/static/media/championenergy.3c8a9172.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "61f1ffff223297fb221ee3f2ea9846d8",
+    "url": "/static/media/firefox-and-edge.61f1ffff.jpg"
+  },
+  {
+    "revision": "1f03ddbfb9bbc970e08cd50b71097287",
+    "url": "/static/media/govt_mp.1f03ddbf.jpg"
+  },
+  {
+    "revision": "4189fe9d8a297005653e14f68daa0978",
+    "url": "/static/media/hp.4189fe9d.png"
+  },
+  {
+    "revision": "b543f233752b58379ae76290083d581e",
+    "url": "/static/media/ideavate.b543f233.png"
+  },
+  {
+    "revision": "e08ced6f3fd1405990f79a01e011c50c",
+    "url": "/static/media/platforuma.e08ced6f.jpg"
+  },
+  {
+    "revision": "8fd6ae2f0922e6ed19909533860bde90",
+    "url": "/static/media/rrr.8fd6ae2f.gif"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "a8b7d5f7e3eba94c038270e23f725656",
+    "url": "/static/media/tata1.a8b7d5f7.png"
+  },
+  {
+    "revision": "be160a556856607344d0b40762e58fea",
+    "url": "/static/media/tstransco.be160a55.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.b2e2003ee55424e8a53aceef13040f0d.js"
+  "/precache-manifest.7f07d712ad7ff9f8aaec07eeda15fa69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
